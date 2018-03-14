d8612fdc91763f46b922cc50724075b4ed1885da
tbgdwjgit <17049185@qq.com>
2018-03-14 14:25:07 +0800

    test

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,12 @@
 __author__ = 'Test-YLL'
-
 # -*- coding:utf-8 -*-
-
-
 
 print('你好，世界！')
 print('Hello world！')
-
-
 print('hello	world')	#注意到	print	是一个函数
+print('在某些情况下，会存在一个隐含的假设，允许你不使 \
+用反斜杠。这一情况即逻辑行以括号开')
+
+x	=	2
+y	=	3
+print(x	!=	y) #返回True
