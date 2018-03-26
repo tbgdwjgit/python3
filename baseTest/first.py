be5dc43024d44d64ae615e4644e2d34089336ab3
tbgdwjgit <1704185@qq.com>
2018-03-26 16:16:55 +0800

    1

--- baseTest/first.py
+++ baseTest/first.py
@@ -66,7 +66,25 @@ def hello(name):
 hello('Alice')
 hello('Bob')
 
+class Dog():
+    '''一次模拟小狗的简单尝试'''
 
+    def __init__(self, name, age):
+        """初始化属性name和age"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """模拟小狗被命令时蹲下"""
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        """模拟小狗被命令时打滚"""
+        print(self.name.title() + " rolled over!")
+
+
+d = Dog('jack',10)
+d.sit()
 
 
 
