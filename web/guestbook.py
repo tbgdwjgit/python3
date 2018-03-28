abacaa66baac8fbd1c0c60d49232447ffbe3a87d
tbgdwjgit <17049185@qq.com>
2018-03-28 17:18:19 +0800

    1

--- web/guestbook.py
+++ web/guestbook.py
@@ -57,10 +57,11 @@ def post():
     # 获取已提交的数据
     name = request.form.get('name')  # 名字
     comment = request.form.get('comment')  # 留言
-    create_at = datetime.now()  # 投稿时间（当前时间）
+    create_at = datetime.datetime.now()  # 投稿时间（当前时间）
     # 保存数据
     save_data(name, comment, create_at)
     # 保存后重定向到首页
+
     return redirect('/')
 
 @application.template_filter('nl2br')
