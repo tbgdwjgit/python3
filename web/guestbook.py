5176d676467bc0f9a66cb437e74ae8f9c2d21e8b
tbgdwjgit <17049185@qq.com>
2018-03-28 16:36:38 +0800

    1

--- web/guestbook.py
+++ web/guestbook.py
@@ -2,6 +2,10 @@ __author__ = 'Test-YLL'
 
 # coding: utf-8
 import shelve
+import datetime
+
+from flask import Flask, request, render_template, redirect, escape, Markup
+application = Flask(__name__)
 
 DATA_FILE = 'guestbook.dat'
 
@@ -36,7 +40,43 @@ def load_data():
     database.close()
     return greeting_list
 
+@application.route('/')
+def index():
+    """首页
+    使用模板显示页面
+    """
+    # return render_template('index.html')
+    # 读取已提交的数据
+    greeting_list = load_data()
+    return render_template('index.html', greeting_list=greeting_list)
 
+@application.route('/post', methods=['POST'])
+def post():
+    """用于提交评论的URL
+    """
+    # 获取已提交的数据
+    name = request.form.get('name')  # 名字
+    comment = request.form.get('comment')  # 留言
+    create_at = datetime.now()  # 投稿时间（当前时间）
+    # 保存数据
+    save_data(name, comment, create_at)
+    # 保存后重定向到首页
+    return redirect('/')
+
+@application.template_filter('nl2br')
+def nl2br_filter(s):
+    """将换行符置换为 br 标签的模板过滤器
+    """
+    return escape(s).replace('\n', Markup('<br>'))
+@application.template_filter('datetime_fmt')
+def datetime_fmt_filter(dt):
+    """使 datetime对象更容易分辨的模板过滤器
+    """
+    return dt.strftime('%Y/%m/%d %H:%M:%S')
+
+if __name__ == '__main__':
+    # 在IP 地址 127.0.0.1 的8000 端口运行应用程序
+    application.run('127.0.0.1', 8000, debug=True)
 
 
 
