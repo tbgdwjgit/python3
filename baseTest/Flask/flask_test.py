ccacf389f7c3d5826ee30acea5b9e3fc4d893e6e
tbgdwjgit <17049185@qq.com>
2018-03-29 11:51:42 +0800

    1

--- baseTest/Flask/flask_test.py
+++ baseTest/Flask/flask_test.py
@@ -1,9 +1,9 @@
 __author__ = 'Test-YLL'
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
+'''
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
@@ -12,6 +12,17 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
+'''
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
 
 
 if __name__ == '__main__':
