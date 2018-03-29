588fb462c016571e9920c5b12fa755da15bdc8a3
tbgdwjgit <17049185@qq.com>
2018-03-29 11:15:01 +0800

    1

--- baseTest/Flask/flask_test.py
+++ baseTest/Flask/flask_test.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World!</h1>'
 
+# 动态路由
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
 
 if __name__ == '__main__':
     app.run(debug=True)
