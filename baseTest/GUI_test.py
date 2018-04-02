fbd5044a83ece07219a9e15492e8303e86e4e963
tbgdwjgit <17049185@qq.com>
2018-04-02 16:51:23 +0800

    1

--- baseTest/GUI_test.py
+++ baseTest/GUI_test.py
@@ -1,1 +1,33 @@
 __author__ = 'Test-YLL'
+'''
+pip install EasyGui
+'''
+import easygui
+
+# easygui.msgbox('wwqwq')
+
+ﬂavor = easygui.choicebox("What is your favorite ice cream ﬂavor?",
+                  choices = ['Vanilla', 'Chocolate', 'Strawberry'] )
+easygui.msgbox ("You picked " + ﬂavor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
