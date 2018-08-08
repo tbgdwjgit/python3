7292245d92dd08bb18ca81073d64fbcc386384c7
tbgdwjgit <17049185@qq.com>
2018-08-08 09:33:25 +0800

    1

---
+++ baseTest/FileReadTest.py
@@ -0,0 +1,17 @@
+__author__ = 'Test-YLL'
+
+# coding:utf-8
+
+#import fileinput
+#for line in fileinput.input('test.md'):
+    #print(line,end='')
+#for line in fileinput.input('test.md'):
+    #print(line,end='')
+
+# f = open('test.txt')
+# for line in f:
+#     print(line,end = '')
+# print(f.tell())
+# f.seek(6)
+# for line2 in f:
+#     print(line2,end = '')
