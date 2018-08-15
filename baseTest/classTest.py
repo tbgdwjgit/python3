6ab16743c70a88614d772e0fd788856acd983eb8
tbgdwjgit <17049185@qq.com>
2018-08-15 17:07:46 +0800

    1

--- baseTest/classTest.py
+++ baseTest/classTest.py
@@ -40,8 +40,21 @@ class HotGirl(Person,GirlTest):#Python和C++一样，支持多继承的语法。
     '''
     def __init__(self,name):
         Person.__init__(self,name)
-        #super(HotGirl,self).__init__(name)
+        # super(HotGirl,self).__init__(name)
         self.age=28
+
+    @staticmethod
+    def staticTest():
+        print("静态方法")
+
+class staticTest:
+    @staticmethod
+    def staticTest():#后面的括号内没有self
+        print("静态方法")
+    @classmethod
+    def bar(cls):#。类方法的参数也没有self，但是必须有cls这个参数
+        print("This is class method bar().")
+        print("bar() is part of class:", cls.__name__)
 
 if __name__ =="__main__":
     cang =Girl("canglaoshi")
@@ -51,6 +64,9 @@ if __name__ =="__main__":
     #kong = HotGirl("cang")
     #kong.breast(85)
     #kong.how()
-    ck = HotGirl('aa')
-    print(ck.age)
-    print(ck.sex)
+    HotGirl("ss").staticTest()
+    staticTest.staticTest()
+    staticTest.bar()
+    # ck = HotGirl('aa')
+    # print(ck.age)
+    # print(ck.sex)
