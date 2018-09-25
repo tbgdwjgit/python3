0577f5d3dd85663926d526407c1dcf314524ce2f
tbgdwjgit <17049185@qq.com>
2018-09-25 16:54:20 +0800

    1

---
+++ baseTest/Socket_Test.py
@@ -0,0 +1,26 @@
+__author__ = 'Test-YLL'
+
+"""
+socket服务端
+"""
+import socket
+
+HOST=''
+PORT=10888
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((HOST,PORT))
+s.listen(1)
+conn,addr = s.accept()
+print('Client\'s Address:',addr)
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    print("Receive Data:",data.decode('utf-8'))
+    conn.send(data)
+conn.close()
+
+"""
+socket客户端
+"""
+
