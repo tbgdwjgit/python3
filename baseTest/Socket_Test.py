33626853769f77267d7dad39cc95ea42e31519a8
tbgdwjgit <17049185@qq.com>
2018-09-27 11:32:56 +0800

    1

--- baseTest/Socket_Test.py
+++ baseTest/Socket_Test.py
@@ -23,4 +23,16 @@ conn.close()
 """
 socket客户端
 """
+import socket
 
+HOST='localhost'
+PORT=10888
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+data = "你好！"
+while data:
+    s.sendall(data.encode('utf-8'))
+    data = s.recv(512)
+    print("Receive form server:\n",data.decode('utf-8'))
+    data = input('please input a info:\n')
+s.close()
