bc7452eebd111f0f75b98aa5de75f4a7e997e3d5
tbgdwjgit <17049185@qq.com>
2018-09-27 19:47:22 +0800

    1

--- baseTest/Socket_Test.py
+++ baseTest/Socket_Test.py
@@ -75,4 +75,22 @@ while data:
     data = input('please input a info:\n')
 s.close()
 
+'''
+socketserver TCP服务端
+'''
+import socketserver
+HOST='localhost'
+PORT=10888
 
+class MyTcpHandler(socketserver.StreamRequestHandler):
+    def handle(self):
+        while True:
+            data = self.request.recv(1024)
+            if not data:
+                Server.shutdown()
+                break
+            print('Receive Data:',data.decode('utf-8'))
+            self.request.send(data)
+        return
+Server = socketserver.TCPServer((HOST,PORT),MyTcpHandler)
+Server.serve_forever()
