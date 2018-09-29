31d2a382a884928a849561093e53875fb94e32b1
tbgdwjgit <17049185@qq.com>
2018-09-29 16:33:57 +0800

    1

--- baseTest/threading_test.py
+++ baseTest/threading_test.py
@@ -2,11 +2,28 @@ __author__ = 'Test-YLL'
 
 # coding:utf-8
 
-import threading
-def thrfun(x,y):
-    for i in range(x,y):
-        print(str(i*i)+';')
-ta = threading.Thread(target=thrfun,args=(1,6))
-tb = threading.Thread(target=thrfun,args=(16,21))
-ta.start()
-tb.start()
+# import threading
+#
+# def thrfun(x,y):
+#     for i in range(x,y):
+#         print(str(i*i)+';')
+# ta = threading.Thread(target=thrfun,args=(1,6))
+# tb = threading.Thread(target=thrfun,args=(16,21))
+# ta.start()
+# tb.start()
+
+import  threading
+
+class myThread(threading.Thread):
+    def __init__(self,mynum):
+        super().__init__()
+        self.mynum = mynum
+
+    def run(self):
+        for i in range(self.mynum,self.mynum+5):
+            print(str(i*i)+';')
+
+ma = myThread(1)
+mb = myThread(16)
+ma.start()
+mb.start()
