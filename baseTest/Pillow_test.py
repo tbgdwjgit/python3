374617b484ebec242e5020b567f62e7f7042b804
tbgdwjgit <17049185@qq.com>
2018-10-08 17:29:31 +0800

    1

--- baseTest/Pillow_test.py
+++ baseTest/Pillow_test.py
@@ -13,8 +13,7 @@ imgb = Image.open('E:\\13.jpg')
 # Image.composite(imga,imgb,mask).show()
 
 from PIL import ImageFilter
-img_output = Image.open('E:\\14.jpg')
-
+img_output = Image.new('RGB',(1024,768),'red')
 b = imga.filter(ImageFilter.GaussianBlur)
 img_output.paste(b,(224,0))
 img_output.show()
