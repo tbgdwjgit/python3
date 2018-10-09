ba4ab5d058ab15bfe776da10f2fa60acf7d63dea
tbgdwjgit <17049185@qq.com>
2018-10-09 10:53:35 +0800

    1

--- baseTest/Pillow_test.py
+++ baseTest/Pillow_test.py
@@ -4,16 +4,31 @@ __author__ = 'Test-YLL'
 
 from PIL import Image
 
-imga = Image.open('E:\\15.jpg')
-imgb = Image.open('E:\\13.jpg')
+# imga = Image.open('E:\\15.jpg')
+# imgb = Image.open('E:\\13.jpg')
+imga = Image.open(r'E:\15.jpg')
+imgb = Image.open(r'E:\13.jpg')
 
 # Image.blend(imga,imgb,0.5).show()
 
 # mask = Image.open('E:\\13.jpg')
 # Image.composite(imga,imgb,mask).show()
 
-from PIL import ImageFilter
+# from PIL import ImageFilter
+# img_output = Image.new('RGB',(1024,768),'red')
+# b = imga.filter(ImageFilter.GaussianBlur)
+# img_output.paste(b,(224,0))
+# img_output.show()
+
+#图片合成
+from PIL import ImageChops
 img_output = Image.new('RGB',(1024,768),'red')
-b = imga.filter(ImageFilter.GaussianBlur)
-img_output.paste(b,(224,0))
-img_output.show()
+# img_output = ImageChops.add(imga,imgb,scale=1.0,offset=0).show()#相加
+# img_output = ImageChops.subtract(imga,imgb,scale=1.0,offset=0).show()#减去
+# img_output = ImageChops.darker(imga,imgb).show()#变暗
+# img_output = ImageChops.lighter(imga,imgb).show()#变亮
+# img_output = ImageChops.multiply(imga,imgb).show()#正片叠底
+# img_output = ImageChops.screen(imga,imgb).show()#屏幕
+# img_output = ImageChops.invert(imga).show()#反相
+# img_output = ImageChops.difference(imga,imgb).show()#比较
+img_output = ImageChops.constant(imga,125).show()#灰度填充
