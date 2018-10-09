07538e741bd719f0befb9ae5b44620a4b732f7e9
tbgdwjgit <17049185@qq.com>
2018-10-09 16:36:41 +0800

    1

--- baseTest/Pillow_test.py
+++ baseTest/Pillow_test.py
@@ -15,26 +15,26 @@ imgb = Image.open(r'E:\13.jpg')
 # Image.composite(imga,imgb,mask).show()
 
 #滤镜
-from PIL import ImageFilter
-w,h = imga.size
-img_output = Image.new('RGB',(2*w,h))
-img_output.paste(imga,(0,0))
-b = imga.filter(ImageFilter.GaussianBlur(5))
-img_output.paste(b,(w,0))
-img_output.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
+# from PIL import ImageFilter
+# w,h = imga.size
+# img_output = Image.new('RGB',(2*w,h))
+# img_output.paste(imga,(0,0))
+# # b = imga.filter(ImageFilter.GaussianBlur(5))
+# # img_output.paste(b,(w,0))
+# # img_output.show()
+#
+# fltrs = []
+# # fltrs.append(ImageFilter.EDGE_ENHANCE)#边缘强化
+# # fltrs.append(ImageFilter.FIND_EDGES)#查找边缘
+# # fltrs.append(ImageFilter.GaussianBlur(4))#高斯模糊
+# fltrs.append(ImageFilter.BLUR)#模糊
+# fltrs.append(ImageFilter.SHARPEN)#锐化
+# fltrs.append(ImageFilter.UnsharpMask(2,150,3))#USM锐化
+#
+# for fltr in fltrs:
+#     r = imga.filter(fltr)
+#     img_output.paste(r,(w,0))
+#     img_output.show()
 
 #图片合成
 # from PIL import ImageChops
@@ -63,3 +63,19 @@ img_output.show()
 #         b = nh.enhance(ratio)
 #         img_output.paste(b,(w,0))
 #         img_output.show()
+
+#画图
+# from PIL import ImageDraw
+#
+# # a = Image.new('RGB',(200,200),'white')
+# a = imga
+# drw = ImageDraw.Draw(a)
+# drw.rectangle((50,50,1000,600),outline='red')
+# drw.text((60,60),'My First Draw',fill='green')
+# # drw.arc((500,500),0,50,fill='yellow')
+# a.show()
+# # a.save(r'E:\15test.jpg')#保存图片
+
+imgc = Image.open(r'E:\中文名称.jpg')#打开图片
+# imgc.show()#展示图片
+# print(imgc.mode,imgc.size,imgc.format)#打印图片信息
