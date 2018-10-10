f48bdea70df7e8852cb9a9b5d8563d3dbf085a25
tbgdwjgit <17049185@qq.com>
2018-10-10 17:11:57 +0800

    1

---
+++ baseTest/ctypes_test.py
@@ -0,0 +1,20 @@
+__author__ = 'Test-YLL'
+
+# coding:utf-8
+
+'''
+使用python中的ctypes模块可以很方便的调用windows的dll（也包括linux下的so等文件）
+'''
+import ctypes
+from ctypes import *
+
+#cdecl调用约定
+# dll = CDLL("Lib.dll")
+# dll = ctypes.cdll.LoadLibrary("Lib.dll")
+
+#stdcall调用约定
+# dll = ctypes.windll.LoadLibrary("Lib.dll")
+dll = ctypes.WinDLL("Lib.dll")
+str = dll.testint()
+# str = dll.testpchar()
+print(str)
