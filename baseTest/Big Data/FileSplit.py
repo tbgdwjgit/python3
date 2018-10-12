80065c4c371b0df0e1febd887164bb01f5c6a29d
tbgdwjgit <17049185@qq.com>
2018-10-12 14:36:22 +0800

    1

--- baseTest/Big Data/FileSplit.py
+++ baseTest/Big Data/FileSplit.py
@@ -9,27 +9,28 @@ import os,os.path,time
 
 def FileSplit(sourceFile,targetFolder):
     sFile = open(sourceFile,'r')
-    number = 100#每个文件保存记录条数
+    number = 500 #每个文件保存记录条数
     dataLine = sFile.readline()
-    tempData = []#缓存列表
+    tempData = [] #缓存列表
     fileNum = 1
     if not os.path.isdir(targetFolder):#目标目录不存在，则创建
         os.mkdir(targetFolder)
-    while dataLine:#有数据
+
+    while dataLine: #有数据
         for row in range(number):
             tempData.append(dataLine)#增加一行数据到列表
             dataLine = sFile.readline()
-            if not dataLine :
+            if not dataLine: #没有数据需要保存
                 break
-            # tFilename = os.path.join(targetFolder,os.path.split(sourceFile)[1]) + str(fileNum) + ".txt")
-            tFilename =''
-            tFile = open(tFilename,'a+') #创建小文件
-            tFile.writelines(tempData) #将临时列表保存到文件中
-            tFile.close()
-            tempData = [] #清空缓存列表
-            print()
-            fileNum += 1 #文件编号
+        tfilename = os.path.join( targetFolder,os.path.split(sourceFile)[1] + str(fileNum) + ".txt")
+        tfile = open(tfilename,'a+') #创建小文件
+        tfile.writelines(tempData) #将临时列表保存到文件中
+        tfile.close()
+        tempData = [] #清空缓存列表
+        print(tfilename +"创建于：" + str(time.ctime()))
+        fileNum += 1 #文件编号
+
     sFile.close()
 
 if __name__ == "__main__":
-    FileSplit('','')
+    FileSplit("SeekView.log","SeekView")
