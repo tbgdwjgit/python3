1a1800189544dc646827aef64596201f9e17177d
tbgdwjgit <17049185@qq.com>
2018-10-24 19:58:30 +0800

    1

--- baseTest/re_test.py
+++ baseTest/re_test.py
@@ -17,6 +17,8 @@ import  re
 
 # m = re.match('fo','fo#d.fdowqqqqqqqqqqqq')
 m = re.search('fo','wwwwwwwwwfo#d.fdowqqqqqqqqqqqq')
+m = re.match('\w+@(\w+\.)?\w+\.com','1eee@163.com')
+
 if m is not None:
     print(m.group())
 
