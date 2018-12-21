97b1ae15f0c148ba0f21c069a59c05aa327ba866
tbgdwjgit <17049185@qq.com>
2018-12-21 14:56:55 +0800

    1

--- baseTest/reptile/pic_Test.py
+++ baseTest/reptile/pic_Test.py
@@ -22,15 +22,13 @@ def craw(url,page):
     pat1='<div id="plist".+?<div class="page clearfix">'
     result1=re.compile(pat1).findall(html1)
     result1=result1[0]
-    pat2='<img width="220" height="220" data-img="1"data-lazy-img="//(.+?\.jpg)">'
+    # pat2='<img width="220" height="220" data-img="1"data-lazy-img="//(.+?\.jpg)">'
+    pat2='<img width="220" height="220" data-img="1" src="//(.+?\.jpg)">'
     imagelist=re.compile(pat2).findall(result1)
     x=1
-    print(imagelist)
     for imageurl in imagelist:
-        imagename='e://test1221//'+str(page) + str(x) + ".jpg"
-        print(imagename)
+        imagename='e:/test1221/'+str(page) + str(x) + ".jpg"
         imageurl="http://"+imageurl
-        print(imageurl)
         try:
             urllib.request.urlretrieve(imageurl,filename=imagename)
         except urllib.error.URLError as e:
