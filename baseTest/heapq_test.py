10661ba458fe37932a50fae564af18b902cc4b4a
tbgdwjgit <17049185@qq.com>
2019-01-08 17:14:44 +0800

    1

--- baseTest/heapq_test.py
+++ baseTest/heapq_test.py
@@ -4,4 +4,6 @@ import heapq
 
 nums = [1,8,2,23,7,-4,18,23,42,37,2]
 print(heapq.nlargest(1,nums))
-print(heapq.nlargest(11,nums))
+print(heapq.nlargest(11,nums))#大到小
+print(heapq.nsmallest(1,nums))
+print(heapq.nsmallest(11,nums))#小到大
